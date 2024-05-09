d55dd94c9d7e9975421b9bf4b83403b5dde97693
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-05-09 20:33:34 +0000

    Initial profile creation and threading setup.

--- appstarter/src/profiles/Profile.hpp
+++ appstarter/src/profiles/Profile.hpp
@@ -1,21 +1,34 @@
 #pragma once
 #include <filesystem>
 #include <string>
+#include <vector>
+#include "../options/environment_vars.hpp"
 
 namespace cwms {
     namespace opendcs {
 
-using std::filesystem::path;
-using std::string;
+
 
 class Profile {
     private:
-        path file;
+        std::filesystem::path file;
+        const std::string app_name;
+        const std::string office;
 
+        Profile(std::filesystem::path profile_file, std::string app_name);
     public:
+        Profile() = delete;
+        ~Profile() = default;
+        Profile(const Profile& other) = default;
+        Profile(Profile&& other) = default;
+        Profile& operator= (const Profile& other) = default;
+        Profile& operator= (Profile &&other) = default;
 
-        const path& get_profile_path();
+        const std::filesystem::path& get_profile_path() const;
+        const std::string get_office() const;
+        const std::string get_app_name() const;
 
+        static std::vector<Profile> from(const environment_vars& vars, const std::filesystem::path& template_file);
 };
 
     }
