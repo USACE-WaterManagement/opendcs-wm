1e3a3b431098f8071040248c1263ccc97a9cc80e
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-04-29 20:53:06 +0000

    Add migration container steps.

---
+++ scripts/migrate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export DATABASE_URL=$JDBC_URL
+export CWMS_OFFICE=LRL
+export DATABASE_TYPE=CWMS
+export DATABASE_DRIVE="oracle.jdbc.driver.OracleDriver"
+export DATATYPE_STANDARD="CWMS"
+export KEYGENERATOR="decodes.sql.OracleSequenceKeyGenerator"
+
+source /opt/opendcs/tsdb_config.sh
+
+cat /dcs_user_dir/user.properties
+
+script -c "manageDatabase -I ${OPENDCS_IMPLEMENTATION} -P /dcs_user_dir/user.properties" <<EOF
+${FLYWAY_USERNAME}
+${FLYWAY_PASSWORD}
+CWMS_20
+CCP
+1
+HQ
+
+${DATABASE_USERNAME}
+${DATABASE_PASSWORD}
+${DATABASE_PASSWORD}
+EOF
