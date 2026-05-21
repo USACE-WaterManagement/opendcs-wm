c52efe0a48d01d12146563b84628c1b8c16c871a
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-05-21 19:06:43 +0000

    Set all required variables.
    (cherry picked from commit 032b7942ab5eb1220e86577f5326eba53781852b)

---
+++ scripts/web.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+export DB_URL=$JDBC_URL
+export DATABASE_TYPE=CWMS-Oracle
+export DATABASE_IMPLEMENTATION=CWMS-Oracle
+export DB_DRIVER_CLASS=oracle.jdbc.driver.OracleDriver
+export DB_MAX_CONNECTIONS=30
+export DB_MAX_IDLE=10
+export DB_MIN_IDLE=5
+export DB_USERNAME=$DCS_USERNAME
+export DB_PASSWORD=$DCS_PASSWORD
+export DB_VALIDATION_QUERY="select 1 from dual"
+
+exec $*
