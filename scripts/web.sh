d956b70001c7cf9b37a8b618190f6afc1ec7c7d2
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-05-21 20:11:29 +0000

    Correct variable names
    (cherry picked from commit 9d5f47163238555fa3a039eb201fe5324e5bc68d)

--- scripts/web.sh
+++ scripts/web.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 export DB_URL=$DATABASE_URL
 export DATABASE_TYPE=CWMS-Oracle
 export DATABASE_IMPLEMENTATION=CWMS-Oracle
@@ -8,8 +7,8 @@ export DB_DRIVER_CLASS=oracle.jdbc.driver.OracleDriver
 export DB_MAX_CONNECTIONS=30
 export DB_MAX_IDLE=10
 export DB_MIN_IDLE=5
-export DB_USERNAME=$DCS_USERNAME
-export DB_PASSWORD=$DCS_PASSWORD
+export DB_USERNAME=$DATABASE_USERNAME
+export DB_PASSWORD=$DATABASE_PASSWORD
 export DB_VALIDATION_QUERY="select 1 from dual"
 
 exec /usr/local/tomcat/bin/catalina.sh run
